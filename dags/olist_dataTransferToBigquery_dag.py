0f5f7848fa1c2d4cee459b62910246dd1ec51e2a
zabdeln4 <53208442+zabdeln4@users.noreply.github.com>
2025-01-03 00:23:31 +0200

    Fix DAG to use existing GCS bucket instead of creating a new one

--- dags/olist_dataTransferToBigquery_dag.py
+++ dags/olist_dataTransferToBigquery_dag.py
@@ -1,5 +1,4 @@
 from airflow import DAG
-from airflow.providers.google.cloud.operators.gcs import GCSCreateBucketOperator
 from airflow.providers.google.cloud.operators.gcs import GCSDeleteObjectsOperator
 from airflow.providers.google.cloud.transfers.postgres_to_gcs import PostgresToGCSOperator
 from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
@@ -16,7 +15,7 @@ DEFAULT_ARGS = {
     'retries': 1
 }
 
-BUCKET_NAME='olist_temp_bucket_ziad'
+BUCKET_NAME='ready-d25-postgres-to-gcs'
 PROJECT_ID='ready-de-25'
 DATASET_ID='olist_ziad'
 
@@ -30,46 +29,39 @@ with DAG(
     catchup=False
 ) as dag:
 
-    # Create GCS bucket for temporary storage
-    create_gcs_bucket = GCSCreateBucketOperator(
-        task_id='create_gcs_bucket',
-        bucket_name=BUCKET_NAME,
-        storage_class='STANDARD',
-        location='US'
-    )
 
-    # # Cleanup bucket
-    cleanup_gcs_bucket = GCSDeleteObjectsOperator(
-        task_id='cleanup_gcs_bucket',
-        bucket_name=BUCKET_NAME,
-        prefix=''  # Specify the prefix or objects to delete
-    )
+    # # # Cleanup bucket
+    # cleanup_gcs_bucket = GCSDeleteObjectsOperator(
+    #     task_id='cleanup_gcs_bucket',
+    #     bucket_name=BUCKET_NAME,
+    #     prefix=''  # Specify the prefix or objects to delete
+    # )
 
     # PostgreSQL to GCS transfer tasks
     postgres_tables = {
         "customers": "SELECT * FROM customers",
-        "geolocations": "SELECT * FROM geolocations",
+        "geolocation": "SELECT * FROM geolocation",
         "order_items": "SELECT * FROM order_items",
-        "order_reviews": "SELECT * FROM order_reviews",
+        "order_reviews": "SELECT review_id, order_id, review_score FROM order_reviews",
         "orders": "SELECT * FROM orders",
         "products": "SELECT * FROM products",
-        "product_category_name_translation": "SELECT * FROM product_category_name_translation",
+        "product_category_name_translation": "SELECT * FROM product_category_name_translation"
     }
 
     for table_name, query in postgres_tables.items():
         export_to_gcs = PostgresToGCSOperator(
             task_id=f'export_{table_name}_to_gcs',
-            postgres_conn_id='postgres_default',
+            postgres_conn_id='postgres_z',
             sql=query,
             bucket=BUCKET_NAME,
-            filename=f'{table_name}.csv',
+            filename=f'ziad/olist/{table_name}.csv',
             export_format='csv',
         )
 
         load_to_bq = GCSToBigQueryOperator(
             task_id=f'load_{table_name}_to_bq',
             bucket=BUCKET_NAME,
-            source_objects=[f'{table_name}.csv'],
+            source_objects=[f'ziad/olist/{table_name}.csv'],
             destination_project_dataset_table=f'{PROJECT_ID}.{DATASET_ID}.{table_name}',
             source_format='csv',
             skip_leading_rows=1,
@@ -77,7 +69,7 @@ with DAG(
             write_disposition='WRITE_TRUNCATE'
         )
 
-        create_gcs_bucket >> export_to_gcs >> load_to_bq
+        export_to_gcs >> load_to_bq
 
     # Fetch API data and load to BigQuery
     api_endpoints = {
@@ -88,7 +80,7 @@ with DAG(
     for table_name, endpoint in api_endpoints.items():
         check_api = HttpSensor(
             task_id=f'check_{table_name}_api',
-            http_conn_id='http_default',
+            http_conn_id='http_z',
             endpoint=endpoint,
             response_check=lambda response: response.status_code == 200,
             poke_interval=5,
@@ -98,13 +90,13 @@ with DAG(
         load_api_to_bq = GCSToBigQueryOperator(
             task_id=f'load_{table_name}_to_bq',
             bucket=BUCKET_NAME,
-            source_objects=[f'{table_name}.csv'],
+            source_objects=[f'ziad/olist/{table_name}.csv'],
             destination_project_dataset_table=f'{PROJECT_ID}.{DATASET_ID}.{table_name}',
             source_format='csv',
             write_disposition='WRITE_TRUNCATE'
         )
 
-        create_gcs_bucket >> check_api >> load_api_to_bq
+        check_api >> load_api_to_bq
 
     # Cleanup bucket after processing
-    cleanup_gcs_bucket.set_upstream([load_to_bq, load_api_to_bq])
+    # cleanup_gcs_bucket.set_upstream([load_to_bq])
